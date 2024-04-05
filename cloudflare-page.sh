ca724dfb51c18588765b6025682b0633627692ca
bangnokia <bangnokia@gmail.com>
2024-04-06 01:53:10 +0700

    wip

--- cloudflare-page.sh
+++ cloudflare-page.sh
@@ -3,5 +3,5 @@
 curl -sL https://dl.static-php.dev/static-php-cli/common/php-8.3.4-cli-linux-x86_64.tar.gz | tar xz
 curl -sL lina.phar https://github.com/bangnokia/lina/releases/latest/download/lina.phar
 
-chmod +x lina.phar
+chmod +x php
 ./php lina.phar build
