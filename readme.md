2a4f1d092b048c7645087999d61f5a7f3f0f4584
bangnokia <bangnokia@gmail.com>
2024-12-24 15:09:02 +0700

    update lina home page

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Lina site
+
+The landing page of Lina, an ultra-lightweight, ultra-fast, and ultra-secure static site generator.
+
+https://lina.daudau.cc
+
+## Development
+```
+composer install ## Install dependencies, only need to run once
+composer serve
+```
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
     <section space-y-24>
         <div mt-12 flex="~ items-center justify-center gap-12">
             <a block
-               px-10 py-6
+               px-10 py-4
                decoration-none
                border="~ 1 solid red-500"
                text-red-500
@@ -29,7 +29,7 @@
                 Get started
             </a>
             <a flex items-center gap-2
-               px-10 py-6
+               px-10 py-4
                decoration-none
                border="~ 1 solid neutral-900"
                text-neutral-900
@@ -41,7 +41,7 @@
                     <path fill="currentColor" fill-rule="evenodd"
                           d="M16 2a14 14 0 0 0-4.43 27.28c.7.13 1-.3 1-.67v-2.38c-3.89.84-4.71-1.88-4.71-1.88a3.71 3.71 0 0 0-1.62-2.05c-1.27-.86.1-.85.1-.85a2.94 2.94 0 0 1 2.14 1.45a3 3 0 0 0 4.08 1.16a2.93 2.93 0 0 1 .88-1.87c-3.1-.36-6.37-1.56-6.37-6.92a5.4 5.4 0 0 1 1.44-3.76a5 5 0 0 1 .14-3.7s1.17-.38 3.85 1.43a13.3 13.3 0 0 1 7 0c2.67-1.81 3.84-1.43 3.84-1.43a5 5 0 0 1 .14 3.7a5.4 5.4 0 0 1 1.44 3.76c0 5.38-3.27 6.56-6.39 6.91a3.33 3.33 0 0 1 .95 2.59v3.84c0 .46.25.81 1 .67A14 14 0 0 0 16 2Z"/>
                 </svg>
-                Source code
+                Github
             </a>
         </div>
 
@@ -69,7 +69,7 @@
         <div grid="~ cols-1 md:cols-2 gap-6">
             @foreach($features as ['name' => $name, 'description' => $description])
                 <div px-6 py-2>
-                    <h2 text="2xl lg:3xl">{{ $name }}</h2>
+                    <h2 text="xl lg:2xl">{{ $name }}</h2>
                     <div>{!! $description !!}</div>
                 </div>
             @endforeach
